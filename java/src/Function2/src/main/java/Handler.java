86b2653a72e9340228956e0f3f5849225cd3c890
Garima-Moodys <Garima.Gupta@moodys.com>
2025-03-28 00:47:33 +0530

    java function modified

--- java/src/Function2/src/main/java/Handler.java
+++ java/src/Function2/src/main/java/Handler.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 public class Handler {
     public Object handler(Object event) {
         Gson gson = new Gson();
-        System.out.println("hello garima2");
+        System.out.println("hello garima1");
 
         return new HashMap();
     }
