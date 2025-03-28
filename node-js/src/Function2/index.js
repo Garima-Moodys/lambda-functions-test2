2e975711956675675b3be4729d5ee8cd243bc812
Garima-Moodys <Garima.Gupta@moodys.com>
2025-03-28 15:00:02 +0530

    java and node-js  function modified for testing

--- node-js/src/Function2/index.js
+++ node-js/src/Function2/index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   // Log the event argument for debugging and for use in local development.
-  console.log("Hello Garima");
+  console.log("Hello");
 
   return {};
 };
