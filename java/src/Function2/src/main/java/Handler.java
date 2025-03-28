86aece65cb69143b2eda2acb1aeed61a15af12dd
Garima-Moodys <Garima.Gupta@moodys.com>
2025-03-28 14:47:48 +0530

    java and node-js  function modified for testing

--- java/src/Function2/src/main/java/Handler.java
+++ java/src/Function2/src/main/java/Handler.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 public class Handler {
     public Object handler(Object event) {
         Gson gson = new Gson();
-        System.out.println("hello garima6");
+        System.out.println("hello garima");
 
         return new HashMap();
     
