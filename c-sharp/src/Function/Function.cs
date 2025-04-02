2d81f43739523fd033152366b37eb56314b93de4
Garima-Moodys <Garima.Gupta@moodys.com>
2025-04-02 13:18:36 +0530

    c-sharp function change

--- c-sharp/src/Function/Function.cs
+++ c-sharp/src/Function/Function.cs
@@ -34,7 +34,6 @@ public class Function
  
             con.Open();
 
-            //test comment
             cmd = new SqlCommand("dbo.Dummy_sp", con)
             {
                 CommandType = CommandType.StoredProcedure
